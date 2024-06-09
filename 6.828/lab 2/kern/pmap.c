c47fdedfe5804937de81b1f1351b94ae9791098e
Vilex1337 <vilesport@gmail.com>
2024-06-09 13:49:18 +0700

    Update

--- 6.828/lab 2/kern/pmap.c
+++ 6.828/lab 2/kern/pmap.c
@@ -263,7 +263,7 @@ page_init(void)
 	size_t i;
 	pages[0].pp_ref = 1;
 	pages[0].pp_link = NULL;
-	for (i = 1; i < npages_basemem; i++) {
+	for (i = 1; i < npages; i++) {
 		pages[i].pp_ref = 0;
 		pages[i].pp_link = page_free_list;
 		page_free_list = &pages[i];
